297f7738887087a323c35966714134eeb12b95d8
mehgellan <baumegg@gmail.com>
2016-08-02 14:36:36 -0700

    successful delete on show page

--- controllers/BooksShowController.js
+++ controllers/BooksShowController.js
@@ -4,16 +4,24 @@ angular.module('libraryApp')
 BooksShowController.$inject=['$http', '$routeParams', '$location'];
 function BooksShowController($http, $routeParams, $location) {
   var vm = this;
-  console.log($routeParams);
   $http({
     method: 'GET',
     url: 'https://super-crud.herokuapp.com/books/' + $routeParams.id
   }).then(function successCb(response) {
-
-    console.log(response.data);
     vm.book = response.data;
     console.log(vm.book);
   }, function errorCb(response) {
     console.log('ERROR', response);
   });
+
+  vm.deleteBook = function() {
+    $http({
+      method: 'DELETE',
+      url: 'https://super-crud.herokuapp.com/books/' + $routeParams.id
+    }).then(function successCb(response) {
+      vm.book = response.data;
+      console.log(vm.book);
+      $location.path('https://super-crud.herokuapp.com/books');
+    });
+  };
 }
