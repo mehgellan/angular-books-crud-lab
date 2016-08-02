2c96a55e6bd6e54347b27d376756273bceed0bc5
mehgellan <baumegg@gmail.com>
2016-08-02 15:21:57 -0700

    successful put with redirect and cancel changes

--- controllers/BooksShowController.js
+++ controllers/BooksShowController.js
@@ -19,9 +19,22 @@ function BooksShowController($http, $routeParams, $location) {
       method: 'DELETE',
       url: 'https://super-crud.herokuapp.com/books/' + $routeParams.id
     }).then(function successCb(response) {
-      vm.book = response.data;
-      console.log(vm.book);
       $location.path('https://super-crud.herokuapp.com/books');
+    }, function errorCb(response) {
+      console.log('ERROR', response);
     });
   };
+
+  vm.editBook = function() {
+    $http({
+      method: 'PUT',
+      url: 'https://super-crud.herokuapp.com/books/' + $routeParams.id,
+      data: vm.book
+    }).then(function successCb(response) {
+      $location.path('https://super-crud.herokuapp.com/books');
+    }, function errorCb(response) {
+      console.log('ERROR', response);
+    });
+  };
+
 }
